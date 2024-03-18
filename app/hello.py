eb40d361f6f6803566cb75094dc0b87c97b68282
bujamini88 <bujamin_i@hotmail.com>
2024-03-18 22:54:09 +0000

    basic flask-blog with css styles und basig html

--- app/hello.py
+++ app/hello.py
@@ -1,13 +1,8 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
-conn = None
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def index():
+    return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
