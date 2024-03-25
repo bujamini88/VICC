2ce9222ca8c0e22aeaade2927803b57cf3cee579
bujamini88 <bujamin_i@hotmail.com>
2024-03-25 18:47:19 +0000

    schema.sql hinzugefügt zum initialisieren der DB

---
+++ schema.sql
@@ -0,0 +1,18 @@
+USE mysql;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    vorname VARCHAR(255) NOT NULL,
+    nachname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    public BOOLEAN NOT NULL DEFAULT TRUE,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
